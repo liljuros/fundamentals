31d7ebd85a684d2acd3703a432b5cae6e1e3a211
erik liljuros <ejn038@gmail.com>
2024-11-14 16:11:27 +0100

    playground

---
+++ playground_strings.py
@@ -0,0 +1,23 @@
+# t = ([1,2], 3)
+# t2 = t *3
+#
+# print(t2)
+# t[0][1] = 5
+# print(t2)
+
+# l = [1,2,3,4,5,6,7,8,9,10]
+# l2 = l[:]
+# print(l2)
+# l2[0] = 100
+# print(l)
+# print(l2)
+# del l2[0]
+# print(l2)
+
+l = [10,20,3,40,50]
+l.append([100,200])
+print(l)
+l.insert(3,42)
+print(l)
+l.insert(2,[1,2,3,4])
+print(l)
