42869fc0f2b999f7fe5f1cdc0df4cbfb55d6e7d9
erik liljuros <ejn038@gmail.com>
2024-11-27 08:22:04 +0100

    exercises string

--- Strings/exercises.py
+++ Strings/exercises.py
@@ -1,11 +1,20 @@
-#print(chr(97))
-s = 'Π, ύ, θ, ω, ν'
-data = "".join(s.split())
-# print(data)
-# print(ord(data[0]))
-for i in data:
-    if i != ',':
-        print(f"For letter: {i}")
-        print(i.upper())
-        print(i.lower())
-        print(hex(ord(i)))
+# #print(chr(97))
+# s = 'Π, ύ, θ, ω, ν'
+# data = "".join(s.split())
+# # print(data)
+# # print(ord(data[0]))
+# for i in data:
+#     if i != ',':
+#         print(f"For letter: {i}")
+#         print(i.upper())
+#         print(i.lower())
+#         print(hex(ord(i)))
+
+# a = input("Give me a number: ")
+# if int(a)%2 == 0:
+#     print(f"The number {a} is even")
+# else:
+#     print(f"The number {a} is uneven")
+
+a, b = input("Give me two numbers, non-zero: ").split()
+print(f"a /b = {(int(a)/int(b)):.2f}")
