83107fb4573d3108b389fb4a58c9720fb642d7e4
erik liljuros <ejn038@gmail.com>
2024-12-04 15:00:09 +0100

    Loops

--- iteration/for_loops.py
+++ iteration/for_loops.py
@@ -6,14 +6,30 @@
 # for i in range(2,11,2):
 #     print(i)
 
-m = [
-    [0,1],
-    [2,3,4,5,6],
-    [7,8,9],
-    [10]
-    ]
+# m = [
+#     [0,1],
+#     [2,3,4,5,6],
+#     [7,8,9],
+#     [10]
+#     ]
 
-for row_index in range(len(m)):
-    for col_index in range(len(m[row_index])):
-        print(f'[{row_index, col_index}] = {m[row_index][col_index]}')
+# for row_index in range(len(m)):
+#     for col_index in range(len(m[row_index])):
+#         print(f'[{row_index, col_index}] = {m[row_index][col_index]}')
 
+data = [10.5, 11.2, 9.8, None, 11.5, None]
+count = 0
+tot = 0
+
+for val in data:
+    if val is not None:
+       count += 1
+       tot += val
+average = tot/count
+print(average)
+print(data)
+for index, val in enumerate(data):
+    if val is None:
+        data[index] = average
+
+print(data)
