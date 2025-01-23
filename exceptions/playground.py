1ce787bae20711a0be7b0efeb40f8a1d392c5a22
Erik Liljuros <erik.liljuros@gdm.se>
2025-01-23 16:50:43 +0100

    exceptions

---
+++ exceptions/playground.py
@@ -0,0 +1,15 @@
+# a = 1
+# b = 0
+# result = a/b
+# print(result)
+
+# ex = ValueError('Name must be at least 5 characters long.')
+# # print(type(ex))
+# # print(ex)
+# # print(repr(ex))
+#
+# name = input('Enter name (5 characters minimum): ')
+#
+# if len(name) < 5:
+#     raise ex
+
