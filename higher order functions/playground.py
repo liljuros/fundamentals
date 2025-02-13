5662c66c450f8ae0a2d13fc57dcb0e97a45eba74
Erik Liljuros <erik.liljuros@gdm.se>
2025-02-13 15:48:08 +0100

    higher order functions

---
+++ higher order functions/playground.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a + b
+
+def greet(name):
+    return f'Hello, {name}!'
+
+#print(add(2,3), greet("John"))
+
+def apply(func, *args):
+    result = func(*args)
+    return result
+
+print(apply(add, 2, 3))
