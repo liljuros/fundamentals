098dffce22324f8cb2aaa5dfceb419c8812058e5
Erik Liljuros <erik.liljuros@gdm.se>
2025-04-17 16:07:40 +0200

    playing around

--- textfiles/playground_files.py
+++ textfiles/playground_files.py
@@ -3,7 +3,7 @@ with open("ips.txt") as file:
     stripped_text = [ips.strip() for ips in every]
     joined_text = ",".join(ips for ips in stripped_text)
 
-with open("output_v2.txt", 'w') as file2:
+with open("output_v2.txt", 'a') as file2:
     file2.write(joined_text)
 
 
