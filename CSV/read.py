8dc7c1b40ebb2723b4c5181dfaa0176ecd352f0e
Erik Liljuros <erik.liljuros@gdm.se>
2025-04-30 16:33:37 +0200

    csv

--- CSV/read.py
+++ CSV/read.py
@@ -14,3 +14,41 @@
 #     reader = csv.reader(f,delimiter=',', quotechar='"')
 #     for row in reader:
 #         print(row)
+import csv
+nasdaq = 'nasdaq.csv'
+census = 'st-2001est-01.csv'
+
+# with open(nasdaq) as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print(row)
+# def parse_nasdaq(f_name):
+#     result = []
+#
+#     with open(f_name) as f:
+#         reader = csv.reader(f)
+#
+#         headers = next(reader)
+#         result.append(headers)
+#
+#         for row in reader:
+#             row[-1] = float(row[-1])
+#             result.append(row)
+#     return result
+#
+# print(parse_nasdaq(nasdaq))
+def parse_census_data(file_name):
+    results = []
+    with open(file_name) as f:
+        reader = csv.reader(f)
+
+        headers = next(reader)
+        results.append(headers)
+        for row in reader:
+            area = row[0]
+            data = row[1:]
+            data = [area] + [int(field.replace(',', ''))for field in data]
+            results.append(data)
+    return results
+
+print(parse_census_data(census))
